e0d842597687886e7d4a3a95fe8d1b0813a55390
Andrew Holcomb <atholcomb@outlook.com>
2026-07-05 21:34:05 -0600

    refactored with comments

--- go-challenges/add2.go
+++ go-challenges/add2.go
@@ -11,16 +11,20 @@ import (
 )
 
 func main() {
+  /* Call the add2() function */
   fmt.Println(add2())
 }
 
 func add2() string {
+  /* Set both variables to their zero value */
   var firstNum, secondNum int
 
+  /* Prompt user to enter two numbers */ 
   fmt.Printf("Enter the 1st number: ")
   fmt.Scanf("%d", &firstNum)
   fmt.Printf("Enter the 2nd number: ")
   fmt.Scanf("%d", &secondNum)
 
+  /* Output the result of the addition for the two numbers passed in */
   return fmt.Sprintf("The total sum equals: %d", firstNum + secondNum)
 }
