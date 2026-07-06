d3f1b6349dec69c0b6c4706a05e590acf86c9187
Andrew Holcomb <atholcomb@outlook.com>
2026-07-05 22:29:01 -0600

    refactored with comments

--- go-challenges/alphaConvert.go
+++ go-challenges/alphaConvert.go
@@ -1,9 +1,8 @@
 /*
-// written by: atholcomb
-// main.go
-// Iterate through the alphabet
-// and output the rune literal of each 
-// letter as it's Unicode representation
+/* authored by: atholcomb
+/* main.go
+/* Iterate through the alphabet and output the rune 
+/* literal of each letter as it's Unicode representation
 */
 
 package main
@@ -13,10 +12,16 @@ import (
 )
 
 func main() {
-  letters := "abcdefghijklmnopqrstuvwxyz"
+  /* declare and store the alphabet */
+  alphabet := "abcdefghijklmnopqrstuvwxyz"
 
-  fmt.Println("Rune\tString")
-  for _, l := range letters {
-    fmt.Printf("%v\t%c\n", l, l)
+  /* create header row */
+  fmt.Println("Rune\tChar")
+  fmt.Println("----\t----")
+  
+  /* iterate over every letter in the alphabet and 
+  convert each letter into its Unicode representation */
+  for _, letter := range alphabet {
+    fmt.Printf("%v\t%c\n", letter, letter)
   }
 }
